63397dda3a3c363091e4b86d7d61cd3b58ac9c82
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2024-09-23 16:23:03 +0545

    changed app

--- app/wsgi.py
+++ app/wsgi.py
@@ -15,6 +15,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
